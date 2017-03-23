a279e4bf4c551f160fd99fb866391c3b68b184de
ChungTV <tranchung927@gmail.com>
2017-03-23 22:33:08 +0700

    Nhập 2 số và in ra các phép tính

---
+++ LearingXamarin/Pheptoan/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pheptoan
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a, b, tong, hieu , tich, thuong;
+
+            Console.WriteLine("\nNhap so a = ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nNhap so b = ");
+            b = int.Parse(Console.ReadLine());
+
+            tong = a + b;
+            hieu = a - b;
+            tich = a * b;
+            thuong = a / b;
+
+            Console.WriteLine("\nKet qua cac phep tinh: ");
+            Console.WriteLine("\nTong a va b la: " + tong);
+            Console.WriteLine("\nHieu a va b la: " + hieu);
+            Console.WriteLine("\nTich a va b la: " + tich);
+            Console.WriteLine("\nThuong a va b la: " + thuong);
+
+            Console.ReadLine();
+
+        }
+    }
+}
